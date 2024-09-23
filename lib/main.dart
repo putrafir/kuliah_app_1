fa65869207f4a8c5656cca6853049ca91a45415a
putrafir <141028423+putrafir@users.noreply.github.com>
2024-09-23 20:09:05 +0700

    baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kuliak_app_1/widget_baru/text_widget.dart';
+import 'package:kuliak_app_1/widget_baru/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -104,15 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[const TextWidget(), const ImageWidget()],
         ),
       ),
       floatingActionButton: FloatingActionButton(
---
+++ lib/widget_baru/text_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Belajar FLutter');
+  }
+}
